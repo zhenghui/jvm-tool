832c36abb688da9efb1f3762a1ba09c3fd56ff57
zhenghui <zhenghui.cjb@taobao.com>
2013-01-14 18:36:13 +0800

    access_flags fix bug

--- src/main/java/zhenghui/jvm/Main.java
+++ src/main/java/zhenghui/jvm/Main.java
@@ -16,15 +16,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ClassLoader cl = new ClassLoader();
         String code = cl.loadClass("d:\\Test.class");
+        //基础信息
         BaseInfoParse baseInfoParse = new BaseInfoParse(code);
         for(String str : baseInfoParse.parseBaseInfo()){
             System.out.println(str);
         }
+        //常量池
         ConstantPoolParse constantPoolParse = new ConstantPoolParse(code);
         ParseResult result = constantPoolParse.pareContantPool();
         for(String str : result.getStrs()){
             System.out.println(str);
         }
+        //访问标记
         result = baseInfoParse.parseAccessFlags(result.getHandle());
         for(String str : result.getStrs()){
             System.out.println(str);
