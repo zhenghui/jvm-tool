2c1369b8cf5eac80cb26de26cc25791aba7da818
zhenghui <zhenghui.cjb@taobao.com>
2013-01-14 15:18:10 +0800

    完成常量池的解析

--- src/main/java/zhenghui/jvm/ClassLoader.java
+++ src/main/java/zhenghui/jvm/ClassLoader.java
@@ -36,7 +36,7 @@ public class ClassLoader {
 
     public static void main(String[] args) throws Exception {
         ClassLoader cl = new ClassLoader();
-        Parse parse = new Parse(cl.loadClass("d:\\Test.class"));
+        ConstantPoolParse parse = new ConstantPoolParse(cl.loadClass("d:\\Test.class"));
         for(String str : parse.pareBaseInfo()){
             System.out.println(str);
         }
