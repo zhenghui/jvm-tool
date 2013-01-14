4eac429f02978f294320d0dac44425fab9e814e4
zhenghui <zhenghui.cjb@taobao.com>
2013-01-14 15:19:50 +0800

    完成常量池的解析

--- src/main/java/zhenghui/jvm/ClassLoader.java
+++ src/main/java/zhenghui/jvm/ClassLoader.java
@@ -34,14 +34,4 @@ public class ClassLoader {
         return sb.toString();
     }
 
-    public static void main(String[] args) throws Exception {
-        ClassLoader cl = new ClassLoader();
-        ConstantPoolParse parse = new ConstantPoolParse(cl.loadClass("d:\\Test.class"));
-        for(String str : parse.pareBaseInfo()){
-            System.out.println(str);
-        }
-        for(String str : parse.pareContantPool()){
-            System.out.println(str);
-        }
-    }
 }
--- src/main/java/zhenghui/jvm/Main.java
+++ src/main/java/zhenghui/jvm/Main.java
@@ -9,7 +9,14 @@ package zhenghui.jvm;
  */
 public class Main {
 
-    public static void main(String [] args){
-
+    public static void main(String[] args) throws Exception {
+        ClassLoader cl = new ClassLoader();
+        ConstantPoolParse parse = new ConstantPoolParse(cl.loadClass("d:\\Test.class"));
+        for(String str : parse.pareBaseInfo()){
+            System.out.println(str);
+        }
+        for(String str : parse.pareContantPool()){
+            System.out.println(str);
+        }
     }
 }
